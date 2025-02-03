61c3a8d5bebfd22ac1e5028d93be38cb6890cb15
RemyIsCool <97812130+RemyIsCool@users.noreply.github.com>
2025-02-02 22:08:09 -0800

    add premake

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+---@diagnostic disable: undefined-global
+workspace "Snake"
+configurations { "Debug", "Release" }
+architecture "x86_64"
+
+project "Snake"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++latest"
+targetdir "bin/%{cfg.buildcfg}"
+
+files { "src/**.cpp", "src/**.h" }
+
+includedirs { "include" }
+libdirs { "lib" }
+links { "raylib" }
+
+filter "system:macosx"
+links {
+    "raylib",
+    "pthread",
+    "dl",
+    "objc"
+}
+linkoptions { "-framework Cocoa", "-framework OpenGL", "-framework IOKit", "-framework CoreVideo" }
+
+filter "system:windows"
+links { "winmm", "gdi32", "opengl32" }
+
+filter "system:linux"
+links { "pthread", "GL", "m" }
+
+filter "configurations:Debug"
+symbols "On"
+
+filter "configurations:Release"
+optimize "On"
