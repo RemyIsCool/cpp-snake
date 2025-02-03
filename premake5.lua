abab5235a10a48557c207b5d6b29d6f4c7a2c370
RemyIsCool <97812130+RemyIsCool@users.noreply.github.com>
2025-02-03 08:17:26 -0800

    fix indentation

--- premake5.lua
+++ premake5.lua
@@ -1,32 +1,32 @@
 ---@diagnostic disable: undefined-global
 workspace "Snake"
-configurations { "Debug", "Release" }
-architecture "ARM64"
+    configurations { "Debug", "Release" }
+    architecture "ARM64"
 
 project "Snake"
-kind "ConsoleApp"
-language "C++"
-cppdialect "C++latest"
-targetdir "bin/%{cfg.buildcfg}"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++latest"
+    targetdir "bin/%{cfg.buildcfg}"
 
-files { "src/**.cpp", "src/**.h" }
+    files { "src/**.cpp", "src/**.h" }
 
-includedirs { "include" }
-libdirs { "lib" }
-links { "raylib" }
+    includedirs { "include" }
+    libdirs { "lib" }
+    links { "raylib" }
 
-filter "system:macosx"
-links { "pthread", "dl", "objc" }
-linkoptions { "-framework Cocoa", "-framework OpenGL", "-framework IOKit", "-framework CoreVideo" }
+    filter "system:macosx"
+        links { "pthread", "dl", "objc" }
+        linkoptions { "-framework Cocoa", "-framework OpenGL", "-framework IOKit", "-framework CoreVideo" }
 
-filter "system:windows"
-links { "winmm", "gdi32", "opengl32" }
+    filter "system:windows"
+        links { "winmm", "gdi32", "opengl32" }
 
-filter "system:linux"
-links { "pthread", "GL", "m" }
+    filter "system:linux"
+        links { "pthread", "GL", "m" }
 
-filter "configurations:Debug"
-symbols "On"
+    filter "configurations:Debug"
+        symbols "On"
 
-filter "configurations:Release"
-optimize "On"
+    filter "configurations:Release"
+        optimize "On"
